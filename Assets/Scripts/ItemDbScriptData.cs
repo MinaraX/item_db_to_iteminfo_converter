7fa3f459092fc8fb1c3acfd871463a8a1bbeb9f8
Kanin Temsrisuk <kaifa2910@msn.com>
2020-03-22 14:52:27 +0700

    Corrected wrong case of getskilllv.

--- Assets/Scripts/ItemDbScriptData.cs
+++ Assets/Scripts/ItemDbScriptData.cs
@@ -117,6 +117,7 @@ public class ItemDbScriptData
         sum = sum.Replace("bonus bDelayrate", "bonus bDelayRate");
         sum = sum.Replace("bonus buseSPRate", "bonus bUseSPrate");
         sum = sum.Replace("bonus bIgnoreMdefRace", "bonus bIgnoreMDefRace");
+        sum = sum.Replace("GetSkillLv", "getskilllv");
 
         //Replace temporary variables in autobonus or bonus_script
         while (sum.Contains("\"+.@") && (sum.Contains("autobonus") || sum.Contains("bonus_script")))
